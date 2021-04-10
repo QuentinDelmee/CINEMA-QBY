39800da18ef7a85ab15e362ffdba291c796e14e4
Quentin Delmee <quentin.delmee.pro@gmail.com>
2021-04-10 22:20:10 +0200

    Création de 10000 utilisateurs avec MDP (gestion des connexions plus tard en BackEnd/FrontEnd) + création de 5 NDS (client employé RH modo admin).

---
+++ BDD/niveaudesecuriteData.sql
@@ -0,0 +1,7 @@
+INSERT INTO niveau_de_securite(id_nds,roles)
+VALUES
+(1,"NDS Client, accès aux utilisations basiques de réservation et achats."),
+(2,"NDS Employé, accès aux utilisations basiques, de ventes et de modifications clients."),
+(3,"NDS RH, accès aux utilisations basiques, ajout et suppression d'employés de NDS 2,3,4."),
+(4,"NDS Modérateur, accès aux utilisations basiques, de ventes et de modifications clients, employé et RH."),
+(5,"NDS Administrateur, accès total.") ;
