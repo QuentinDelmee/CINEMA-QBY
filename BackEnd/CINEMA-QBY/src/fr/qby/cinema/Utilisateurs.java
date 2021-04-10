5e45dae440df44fa0557cd94dfe99e5092a3a76d
Quentin Delmee <quentin.delmee.pro@gmail.com>
2021-04-10 20:35:08 +0200

    Starting Pseudo Creation Script for Automated Filling of DB

---
+++ BackEnd/CINEMA-QBY/src/fr/qby/cinema/Utilisateurs.java
@@ -0,0 +1,12 @@
+package fr.qby.cinema;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Utilisateurs {
+
+}
