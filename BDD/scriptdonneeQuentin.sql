1303f5f0f99be060808b7765ae85c46158e35545
Quentin Delmee <quentin.delmee.pro@gmail.com>
2021-04-10 10:48:06 +0200

    Création d'un script java pour nous aider à remplir automatiquement la BDD de personnes fictives

--- BDD/scriptdonneeQuentin.sql
+++ BDD/scriptdonneeQuentin.sql
@@ -1,7 +1,7 @@
 INSERT INTO personnes (nom, prenom, date_de_naissance,genre, adresse,e_mail)
  VALUES
- ('XXXXX','Bédiss','2003/01/22','H','42 rue de la vie, 424242 Paradis', 'bediss@toto.io'),
- ('YYYYY','Yohann','2003/05/21','H','42 rue de la mort, 666666 Enfer', 'yohann@toto.io'),
- ('Delmée','Quentin','1991/01/22','H','42 rue de la terre, 012345 Galaxie', 'quentin@toto.io'),
- ('ZZZZZ','Holy','2005/01/22','F','42 rue de vénus, 22222 Vénus City', 'holy@toto.io'),
- ('AAAAA','Brieuc','2002/01/22','H','42 rue de jupiter, 11111 Jupiter Town', 'brieuc@toto.io');
+ ('BÃ©diss','BÃ©diss','2003/01/22','H','42 rue de la vie, 424242 Paradis', 'bediss@toto.io'),
+ ('Yohann','Yohann','2003/05/21','H','42 rue de la mort, 666666 Enfer', 'yohann@toto.io'),
+ ('Quentin','Quentin','1991/01/22','H','42 rue de la terre, 012345 Galaxie', 'quentin@toto.io'),
+ ('Holy','Holy','2005/01/22','F','42 rue de vÃ©nus, 22222 VÃ©nus City', 'holy@toto.io'),
+ ('Brieuc','Brieuc','2002/01/22','H','42 rue de jupiter, 11111 Jupiter Town', 'brieuc@toto.io');
