015ff3f8213d950d1ba845d106008707b9360d68
Quentin Delmee <quentin.delmee.pro@gmail.com>
2021-04-21 17:03:21 +0200

    Add Quentin : ajout dossier js css img

---
+++ FrontEnd/js/script_q.js
@@ -0,0 +1,28 @@
+"use strict"
+
+var currentMDP = "test" ;
+
+function formPseudo()
+{
+    let newP = document.getElementById(newpseudo).value ;
+    let newE = document.getElementById(newemail).value ;
+
+    console.log("NewP:" +newP) ;
+    console.log("NewE:"+newE) ;
+
+
+
+
+    return false ;
+}
+
+
+function formInfo()
+{
+
+}
+
+function formMdP()
+{
+
+}
