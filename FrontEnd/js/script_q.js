7475ed9747182e12a28c1340e423225256cd57d5
Quentin Delmee <quentin.delmee.pro@gmail.com>
2021-04-22 09:40:38 +0200

    Add Q : commit to pull yohann changes

--- FrontEnd/js/script_q.js
+++ FrontEnd/js/script_q.js
@@ -1,19 +1,20 @@
-"use strict"
+"use strict" ;
 
 var currentMDP = "test" ;
 
+
 function formPseudo()
 {
-    let newP = document.getElementById(newpseudo).value ;
-    let newE = document.getElementById(newemail).value ;
+    let newP = document.getElementById('newpseudo').value ;
+    let newE = document.getElementById('newemail').value ;
+    let mdp = document.getElementById('mdp').value ;
+
 
     console.log("NewP:" +newP) ;
     console.log("NewE:"+newE) ;
 
 
-
-
-    return false ;
+    return false;
 }
 
 
