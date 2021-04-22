ba24d36f6e8712110d90e2ffbe1f55cd72158be6
Yohann <migliorini-yohannIB@outlook.fr>
2021-04-22 16:58:18 +0200

    Commit général de la journée 22/04

---
+++ FrontEnd/js/quizz_y.js
@@ -0,0 +1,14 @@
+"use strict"
+
+init();
+
+function init(){
+
+};
+
+function bringDictionnary(numberQuestion){
+    let dict = {
+        "Qui prononce cette phrase dans le film Fight Club « Les choses que l’on possède finissent par nous posséder » ?": "Brad Pitt";
+    };
+}
+
