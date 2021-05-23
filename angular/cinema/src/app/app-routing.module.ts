f87f3ebcaa5d932c7c705d16900741c4296c1ff5
Quentin Delmee <quentin.delmee.pro@gmail.com>
2021-05-23 13:15:23 +0200

    Add Q : Angular Routing Basic Page Done !

--- angular/cinema/src/app/app-routing.module.ts
+++ angular/cinema/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { DataComponent } from './data/data.component';
+import { HowToComeComponent } from './how-to-come/how-to-come.component';
+import { MovieComponent } from './movie/movie.component';
+import { ProfilComponent } from './profil/profil.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { TimeTableComponent } from './time-table/time-table.component';
+
+const routes: Routes = [
+  { path: 'home-page', component: HomeComponent },
+  { path: 'data-page', component: DataComponent },
+  { path: 'how-to-come-page', component: HowToComeComponent },
+  { path: 'movie-page', component: MovieComponent },
+  { path: 'profil-page', component: ProfilComponent },
+  { path: 'reservation-page', component: ReservationComponent },
+  { path: 'signin-page', component: SignInComponent },
+  { path: 'signup-page', component: SignUpComponent },
+  { path: 'timetable-page', component: TimeTableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
