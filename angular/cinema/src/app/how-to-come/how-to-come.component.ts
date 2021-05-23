308ae164f43a9e83dcf82cda6d2febe91dfc7af9
Quentin Delmee <quentin.delmee.pro@gmail.com>
2021-05-23 15:00:43 +0200

    Add Q : Routing in Angular

---
+++ angular/cinema/src/app/how-to-come/how-to-come.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-how-to-come',
+  templateUrl: './how-to-come.component.html',
+  styleUrls: ['./how-to-come.component.scss']
+})
+export class HowToComeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
