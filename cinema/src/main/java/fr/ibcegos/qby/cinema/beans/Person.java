4183f055ce000987608eb57ff5f992736970a818
Bédiss <nahdibediss@yahoo.fr>
2021-05-25 15:33:22 +0200

    Add Package BEANS

--- cinema/src/main/java/fr/ibcegos/qby/cinema/beans/Person.java
+++ cinema/src/main/java/fr/ibcegos/qby/cinema/beans/Person.java
@@ -7,13 +7,27 @@ package fr.ibcegos.qby.cinema.beans;
 //Vers le la ligne "120" il y a la table SQL y correspondant
 
 // Class "util"
-import java.util.Date;  
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;  
 
 // Création des Class Identiques au Bases de données
 	// Classe "person"
-
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Person {
-	private int  id_person;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer  id_person;
 	private String family_name;
 	private String first_name; // on ne devrait pas donner un last name pour avoir un nom complet ??
 	private Date date;   	
@@ -24,7 +38,7 @@ public class Person {
 	
 	
 	// Constructeur avec paramètre
-	public Person(int id_person, String family_name, String first_name, Date date, String genre, String city, String e_mail) {
+	public Person(Integer id_person, String family_name, String first_name, Date date, String genre, String city, String e_mail) {
 		this.id_person = id_person;
 		this.family_name = family_name;
 		this.first_name = first_name;
@@ -33,96 +47,25 @@ public class Person {
 		this.city = city;
 		this.e_mail = e_mail;
 	}
-
-
-	// Constructeur par défault
-	public Person() {
-	}
 	
+	// Constructeur avec paramètre sauf id
+		public Person(String family_name, String first_name, Date date, String genre, String city, String e_mail) {
+			this.family_name = family_name;
+			this.first_name = first_name;
+			this.date = date;
+			this.genre = genre;
+			this.city = city;
+			this.e_mail = e_mail;
+		}
+
+
 	
 	//Méthode toString pour accéder aux valeurs 
 	@Override
 	public String toString() {
-		return "L'identifiant : " + id_person 
-				+ " de" + genre
-				+ " " + family_name 
-				+ " " + first_name 
-				+ "né(e) le " + date 
-				+ " à " + city 			
-				+ "e_mail=" + e_mail + "]";
+		return "Person [id_person=" + id_person + ", family_name=" + family_name + ", first_name=" + first_name
+				+ ", date=" + date + ", genre=" + genre + ", city=" + city + ", e_mail=" + e_mail + "]";
 	}
-
-//************************************************************************************//
-	// Gettres (pour récupérer-afficher nos attributs de class )
-//et   Settres (pour apporter les modifictaions le ac échants) 
-	
-	public int getId_person() {
-		return id_person;
-	}
-
-
-	public void setId_person(int id_person) {
-		this.id_person = id_person;
-	}
-	
-	public String getFamily_name() {
-		return first_name;
-	}
-
-
-	public void setFamily_name(String first_name) {
-		this.first_name = first_name;
-	}
-
-	public String getFirst_name() {
-		return first_name;
-	}
-
-
-	public void setFirst_name(String first_name) {
-		this.first_name = first_name;
-	}
-
-
-	public Date getDate() {
-		return date;
-	}
-
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-
-	public String getGenre() {
-		return genre;
-	}
-
-
-	public void setGenre(String genre) {
-		this.genre = genre;
-	}
-
-
-	public String getCity() {
-		return city;
-	}
-
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-
-	public String getE_mail() {
-		return e_mail;
-	}
-
-
-	public void setE_mail(String e_mail) {
-		this.e_mail = e_mail;
-	}
-	
 }
 
 /* Ci-dessous la Table y correpondant
