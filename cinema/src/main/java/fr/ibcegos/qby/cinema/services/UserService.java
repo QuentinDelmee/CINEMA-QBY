74307a26475e7f1761afc1949a9b5a91d776a1a7
Bédiss <nahdibediss@yahoo.fr>
2021-05-25 16:56:43 +0200

    Add Package Services

--- cinema/src/main/java/fr/ibcegos/qby/cinema/services/UserService.java
+++ cinema/src/main/java/fr/ibcegos/qby/cinema/services/UserService.java
@@ -1,5 +1,61 @@
 package fr.ibcegos.qby.cinema.services;
 
-public interface UserService {
+import java.util.List;
 
+import fr.ibcegos.qby.cinema.beans.User;
+
+
+public interface UserService {
+	//////////
+	//CREATE//
+	//////////
+
+	/**
+	 * Créer un utilisateur
+	 * @param user
+	 */
+	public void create(User user);
+	
+		
+	//////////
+	// GET  //
+	//////////
+	/**
+	 * Récupérer un utilisateur par son id
+	 * @param id_user
+	 * @return
+	 */
+	public User getUserId(Integer id_user);
+	
+	/**
+	 * Retourne toutes les utilisateurs
+	 * @return
+	 */
+	public List<User> getAllUser();
+		
+	//////////
+	//UPDATE//
+	//////////
+	/**
+	 * Modifie un utilisateur
+	 * @param user
+	 */
+	
+	public void update(User user);
+		
+	//////////
+	//DELETE//
+	//////////
+	/**
+	 * Supprime un utilisateur
+	 * @param user
+	 */
+	public void delete(User user);
+	
+	
+	/**
+	 * Supprime un utilisateur par son id
+	 * @param id_user
+	 */
+	public void deleteById(Integer id_user);
 }
