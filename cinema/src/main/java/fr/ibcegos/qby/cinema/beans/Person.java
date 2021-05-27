3f223c2a5bd4a8163e8c68c608a8508f27ab775d
Quentin Delmee <quentin.delmee.pro@gmail.com>
2021-05-27 17:24:59 +0200

    Add Q : Final Commit Merge of the Day ! BDD IS WORKING !!!

--- cinema/src/main/java/fr/ibcegos/qby/cinema/beans/Person.java
+++ cinema/src/main/java/fr/ibcegos/qby/cinema/beans/Person.java
@@ -20,54 +20,26 @@ import lombok.Setter;
 public class Person {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-<<<<<<< HEAD
 	private Integer  id;
 	private String familyName;
 	private String firstName; // on ne devrait pas donner un last name pour avoir un nom complet ??
-	private Date date;   	
-=======
-	private Integer  idperson;
-	private String familyname;
-	private String firstname; // on ne devrait pas donner un last name pour avoir un nom complet ??
 	private LocalDate date;   	
->>>>>>> a874e36af9c68ac8c70804b9e8146b3906c1003d
 	private String genre;
 	private String city;
 	private String email;
 	
 	
 	
-<<<<<<< HEAD
-	public Person(String familyName, String firstName, Date date, String genre, String city, String email) {
+
+	public Person(String familyName, String firstName, LocalDate date, String genre, String city, String email) {
 		super();
 		this.familyName = familyName;
 		this.firstName = firstName;
-=======
-	// Constructeur avec paramètre
-	public Person(Integer idperson, String familyname, String firstname, LocalDate date, String genre, String city, String email) {
-		this.idperson = idperson;
-		this.familyname = familyname;
-		this.firstname = firstname;
->>>>>>> a874e36af9c68ac8c70804b9e8146b3906c1003d
 		this.date = date;
 		this.genre = genre;
 		this.city = city;
 		this.email = email;
 	}
-<<<<<<< HEAD
-=======
-	
-	// Constructeur avec paramètre sauf id
-		public Person(String familyname, String firstname, LocalDate date, String genre, String city, String email) {
-			this.familyname = familyname;
-			this.firstname = firstname;
-			this.date = date;
-			this.genre = genre;
-			this.city = city;
-			this.email = email;
-		}
->>>>>>> a874e36af9c68ac8c70804b9e8146b3906c1003d
-
 
 
 	@Override
