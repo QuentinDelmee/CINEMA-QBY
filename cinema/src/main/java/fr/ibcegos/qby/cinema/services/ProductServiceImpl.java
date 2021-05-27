9376ab66f214d27dde788df7ed936f0c26465ac1
Bédiss <nahdibediss@yahoo.fr>
2021-05-27 15:55:49 +0200

    Add Controllers : Product, Security Levet et User

--- cinema/src/main/java/fr/ibcegos/qby/cinema/services/ProductServiceImpl.java
+++ cinema/src/main/java/fr/ibcegos/qby/cinema/services/ProductServiceImpl.java
@@ -12,31 +12,48 @@ public class ProductServiceImpl implements ProductService{
 	@Autowired
 	ProductDAO productDAO;
 	
+	//////////
+	//CREATE//
+	//////////
 	@Override
 	public void create(Product product) {
 		productDAO.save(product);
 	}
-
+	
+	//////////
+	// GET  //
+	//////////
+	//Récupérer un produit par son id
 	@Override
 	public Product getProductById(Integer id_product) {
 		return productDAO.findById(id_product).orElse(null);
 	}
-
+	//Récupérer tous les produits
 	@Override
 	public List<Product> getAllProduct() {
 		return (List<Product>) productDAO.findAll();
 	}
-
+	
+	//////////
+	//UPDATE//
+	//////////
 	@Override
 	public void update(Product product) {
 		productDAO.save(product);		
 	}
-
+	
+	
+	//////////
+	//DELETE//
+	//////////
+	//Effacer tous les produits
+	
 	@Override
 	public void delete(Product product) {
 		productDAO.delete(product);	
 	}
 
+	//Effacer un produit par son id
 	@Override
 	public void deleteProductById(Integer id_product) {
 		productDAO.deleteById(id_product);
--- cinema/src/main/java/fr/ibcegos/qby/cinema/services/Security_LevelServiceImpl.java
+++ cinema/src/main/java/fr/ibcegos/qby/cinema/services/Security_LevelServiceImpl.java
@@ -13,34 +13,49 @@ public class Security_LevelServiceImpl implements Security_LevelService{
 	@Autowired
 	Security_LevelDAO security_LevelDAO;
 	
-	
+	//////////
+	//CREATE//
+	//////////	
 	@Override
 	public void create(Security_Level securityLevel) {
-		security_LevelDAO.save(securityLevel);
-		
+		security_LevelDAO.save(securityLevel);	
 	}
 
+	//////////
+	// GET  //
+	//////////
+	//Récupérer un niveau de sécurité par son id
 	@Override
 	public Security_Level getSecurityLevelById(Integer id_sl) {
 		return security_LevelDAO.findById(id_sl).orElse(null);
 	}
 
+	//Récupérer tous les niveaux de sécurité
 	@Override
 	public List<Security_Level> getAllSecurityLevel() {
 		return (List<Security_Level>) security_LevelDAO.findAll();
 	}
 
+	//////////
+	//UPDATE//
+	//////////
 	@Override
 	public void update(Security_Level securityLevel) {
 		security_LevelDAO.save(securityLevel);		
 	}
 
+	
+	//////////
+	//DELETE//
+	//////////
+	//Effacer un niveau de sécurité par son id
 	@Override
 	public void delete(Security_Level securityLevel) {
 		security_LevelDAO.deleteAll();
 		
 	}
 
+	//Effacer tous les niveaux de sécurité
 	@Override
 	public void deleteById(Integer id_sl) {
 		security_LevelDAO.deleteById(id_sl);
