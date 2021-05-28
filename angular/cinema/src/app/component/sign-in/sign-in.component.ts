c462c52ce51ad8720626477d584969e663306eab
Quentin Delmee <quentin.delmee.pro@gmail.com>
2021-05-28 15:15:07 +0200

    Add Q : debug form

--- angular/cinema/src/app/component/sign-in/sign-in.component.ts
+++ angular/cinema/src/app/component/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
  import { Component, OnInit } from '@angular/core';
- import {FormControl, Validators, FormGroup, ReactiveFormsModule, NgForm} from '@angular/forms';
+ import { FormControl, Validators, FormGroup  } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
