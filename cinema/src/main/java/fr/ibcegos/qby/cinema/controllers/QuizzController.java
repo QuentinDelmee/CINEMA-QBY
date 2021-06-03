da9edf5bbb69b87b47d0633212d6edacdd282c5d
Yohann <migliorini-yohannIB@outlook.fr>
2021-06-03 11:49:32 +0200

    Controller quizz

--- cinema/src/main/java/fr/ibcegos/qby/cinema/controllers/QuizzController.java
+++ cinema/src/main/java/fr/ibcegos/qby/cinema/controllers/QuizzController.java
@@ -1,5 +1,22 @@
 package fr.ibcegos.qby.cinema.controllers;
 
-public class QuizzController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.ibcegos.qby.cinema.beans.Quizz;
+import fr.ibcegos.qby.cinema.services.QuizzService;
+
+@RestController
+public class QuizzController {
+	@Autowired
+	private QuizzService qservice;
+	
+	
+	@GetMapping("/REST/quizz")
+	public List<Quizz> getAll() {
+		return qservice.findAll();
+	}
 }
